b2f18fc8141a215272e90380fa16059ca1810134
cyliong <liongtheprogrammer@gmail.com>
2022-01-18 21:37:04 +0800

    Declare constants in WeatherService

--- Weather/Service/WeatherService.swift
+++ Weather/Service/WeatherService.swift
@@ -1,4 +1,7 @@
 class WeatherService {
+    private static let apiKey = ""
+    private static let serviceURL =
+        "https://api.openweathermap.org/data/2.5/weather?appid=\(apiKey)&units=metric"
     
     func getWeather(cityName: String) -> Weather? {
         nil
