32658a40ad0c0b758b68914052f53dc617061295
cyliong <liongtheprogrammer@gmail.com>
2022-03-03 22:22:25 +0800

    Create an outlet for the weather stack view

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -4,6 +4,8 @@ class ViewController: UIViewController {
 
     @IBOutlet private var emptyDataLabel: UILabel!
     
+    @IBOutlet private var weatherStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
