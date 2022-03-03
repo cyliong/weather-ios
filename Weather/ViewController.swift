7b4c73037399f17169d5497bb0bc8296e6980203
cyliong <liongtheprogrammer@gmail.com>
2022-03-03 20:23:02 +0800

    Create an outlet for the empty data label

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var emptyDataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
