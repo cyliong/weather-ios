cb3a99edd91772f078ffcfdea585a7264be852ee
cyliong <liongtheprogrammer@gmail.com>
2022-03-07 22:20:13 +0800

    Check web service errors

--- Weather/Service/WeatherService.swift
+++ Weather/Service/WeatherService.swift
@@ -12,11 +12,20 @@ class WeatherService {
         let url = getWeatherURL(cityName: cityName)!
         let task = URLSession.shared.dataTask(with: url) {
             (data, response, error) in
-            print(String(decoding: data!, as: UTF8.self))
             
-            // TODO: Parse JSON
-            
-            completionHandler(nil)
+            let weather: Weather?
+            if let data = data,
+               error == nil,
+               let httpResponse = response as? HTTPURLResponse,
+               (200...299).contains(httpResponse.statusCode)
+            {
+                // TODO: Parse JSON
+                print(String(decoding: data, as: UTF8.self))
+                weather = nil
+            } else {
+                weather = nil
+            }
+            completionHandler(weather)
         }
         task.resume()
     }
